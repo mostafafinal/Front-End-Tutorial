4bafd6e8e84deaed9ac39ead5e13a4f4eafd69b1
Mostafa <mostafafinal55@gmail.com>
2024-07-17 05:24:50 +0300

    Calorie Tracher react learning project

---
+++ react/calorie-tracker/src/App.jsx
@@ -0,0 +1,18 @@
+import CalorieRecord from "./components/CalorieRecord";
+function App() {
+  return (
+    <div className="App">
+      <h2>
+        Let's try to test the lagging issue again after disabling the ESLint
+        extension
+      </h2>
+      <p>
+        Now I think that the tiny lagging that happens it's related to the code
+        intellisense extension
+      </p>
+      <CalorieRecord></CalorieRecord>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/calorie-tracker/src/components/CalorieRecord.jsx
@@ -0,0 +1,12 @@
+function CalorieRecord() {
+  return (
+    <ul>
+      <li>340 Calories</li>
+      <li>Beakfast</li>
+      <li>Eggs + Beans</li>
+      <li>March 6, 2024</li>
+    </ul>
+  );
+}
+
+export default CalorieRecord;
