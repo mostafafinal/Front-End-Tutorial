7aee74a36e638c1389e0116badd263dc08c2e5f2
Mostafa <mostafafinal55@gmail.com>
2024-07-22 12:14:21 +0300

    Typescript Learnig tasks

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,84 @@
-console.log(Math.floor(15.31));
+// Task 1
+function calculate(numOne: number, numTwo: number) {
+  return numOne + numTwo;
+}
+
+console.log(calculate(10, 20)); // 30
+console.log(calculate("10", "20")); // We Don't Need This To Work
+console.log(calculate(+true, +true)); // 2
+
+// Task 2
+function printInfo(valueOne: string | number, valueTwo: string | number) {
+  return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+
+console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
+
+// Task 3
+let arr: (string | number | boolean[] | (string | (string | number)[])[])[] = [
+  "string",
+  10,
+  [true, false],
+  ["string", ["string", 10]],
+];
+
+// Task 4
+function reportErrors(username, age: number) {
+  let rank = "Professor";
+  return `Username: ${username}`;
+  console.log("We Will Not Reach Here");
+}
+
+console.log(reportErrors("Elzero", 40));
+
+// Task 5
+let nothing;
+let theName: string = "Elzero";
+function showMyDetails(a = "", b = "", c) {
+  return `${a}${b}${c}`;
+}
+
+// Replace ???? With The Available Variables As Argument To Get The Result
+console.log(showMyDetails(nothing, nothing, theName)); // Elzero
+
+// Task 6
+function showMsg(user?: string, age?: number | string, country?: string) {
+  return `${user}${age}${country}`;
+}
+
+console.log(showMsg());
+console.log(showMsg("Elzero"));
+console.log(showMsg("Elzero", 40));
+console.log(showMsg("Elzero", "40", "Egypt"));
+
+// Task 7
+// Write The Function Here
+function printInConsole(...param) {
+  console.log(`The Value Is ${param} And Type Is ${typeof param}`);
+}
+// Using The Function => Do Not Edit
+console.log(printInConsole(1, 2, 3, 4, 5));
+console.log(printInConsole("A", "B", "C"));
+console.log(printInConsole(true, false, false, true, true));
+
+// Output
+/*
+The Value Is 1 And Type Is number
+The Value Is 2 And Type Is number
+The Value Is 3 And Type Is number
+The Value Is 4 And Type Is number
+The Value Is 5 And Type Is number
+Done
+The Value Is A And Type Is string
+The Value Is B And Type Is string
+The Value Is C And Type Is string
+Done
+The Value Is true And Type Is boolean
+The Value Is false And Type Is boolean
+The Value Is false And Type Is boolean
+The Value Is true And Type Is boolean
+The Value Is true And Type Is boolean
+Done
+*/
